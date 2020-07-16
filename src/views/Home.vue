0a9efa9b57d19c86de125b6501ad7fa9a337c8d1
Andrey Itiligator <a.e.itiligator@gmail.com>
2020-07-16 16:39:02 +0700

    Допилил главную с красивыми app-bar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <v-toolbar dense color="primary">
+        <v-app-bar elevate-on-scroll dense fixed color="primary">
             <v-menu
             top
             close-on-content-click
@@ -39,7 +39,11 @@
             </v-btn>
             <v-spacer></v-spacer>
             {{ username }}
-        </v-toolbar>
+        </v-app-bar>
+
+<!-- Тулбар нужен чтобы app-bar не перекрывал содержимое страницы -->
+        <v-toolbar dense flat fixed color="primary"></v-toolbar>
+
         <v-simple-table>
             <template v-slot:default>
                 <thead>
@@ -53,11 +57,58 @@
                     <tr><td>АПА</td><td>300</td></tr>
                     <tr><td>Балтийский портер</td><td>100</td></tr>
                     <tr><td>IPA</td><td>50</td></tr>
+                    <tr><td>IPA</td><td>50</td></tr>
+                    <tr><td>IPA</td><td>50</td></tr>
+                    <tr><td>IPA</td><td>50</td></tr>
+                </tbody>
+            </template>
+        </v-simple-table>
+        <v-simple-table>
+            <template v-slot:default>
+                <thead>
+                    <tr>
+                        <th>Статус выполнения</th>
+                        <th>Оплата ПЛАН</th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Запланирован</td>
+                        <td>2000</td>
+                        <td><v-btn>Начать<br/>визит</v-btn></td>
+                        <td><v-btn>Переход в<br/>клиента</v-btn></td>
+                    </tr>
+                    <tr>
+                        <td>Запланирован</td>
+                        <td>3000</td>
+                        <td><v-btn>Начать<br/>визит</v-btn></td>
+                        <td><v-btn>Переход в<br/>клиента</v-btn></td>
+                    </tr>
+                    <tr>
+                        <td>Окончен</td>
+                        <td>6000</td>
+                        <td></td>
+                        <td><v-btn>Переход в<br/>клиента</v-btn></td>
+                    </tr>
+                    <tr>
+                        <td>Окончен</td>
+                        <td>1000</td>
+                        <td></td>
+                        <td><v-btn>Переход в<br/>клиента</v-btn></td>
+                    </tr>
+                    <tr>
+                        <td>Окончен</td>
+                        <td>500</td>
+                        <td></td>
+                        <td><v-btn>Переход в<br/>клиента</v-btn></td>
+                    </tr>
                 </tbody>
             </template>
         </v-simple-table>
 
-        <v-toolbar dense bottom color="primary">
+        <v-app-bar dense flat bottom fixed color="primary">
             <v-btn>
                 На неделю/на
                 <br/>
@@ -65,9 +116,13 @@
             </v-btn>
             <v-spacer></v-spacer>
             <v-btn>
-                ДОБАВИТЬ ВИЗИТ
+                ДОБАВИТЬ
+                <br/>
+                ВИЗИТ
             </v-btn>
-        </v-toolbar>
+        </v-app-bar>
+        <!-- Тулбар нужен чтобы app-bar не перекрывал содержимое страницы -->
+        <v-toolbar dense flat bottom fixed color="primary"></v-toolbar>
     </div>
 </template>
 
